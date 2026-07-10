25384197de4eed0a21b0f2cbf4193bbb0b0e9bfb
HiddenGold <104958576+HiddenG0ld@users.noreply.github.com>
2026-07-10 21:53:24 +0800

    Update dashboard.php

--- student-side-main/dashboard.php
+++ student-side-main/dashboard.php
@@ -31,7 +31,7 @@
 
                 <div class="profile-section">
                     <button class="profile-btn" id="profileBtn">
-                        <img src="img/profile-test.jpg" alt="Profile" class="profile-pic" onerror="this.onerror=null; this.src='img/default-profile.png';">
+                        <img src="../profiles-pictures/profile-test.jpg" alt="Profile" class="profile-pic" onerror="this.onerror=null; this.src='img/default-profile.png';">
                     </button>
 
                     <div class="dropdown-menu" id="dropdownMenu">
