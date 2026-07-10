ff20f380d37b88e7447463ad2ef9b66e41220305
Te0866 <johann_te@dlsu.edu.ph>
2026-07-10 18:04:21 +0800

    Update script.js

--- login-side-main/js/script.js
+++ login-side-main/js/script.js
@@ -13,23 +13,23 @@ toggleButton.addEventListener("click",() => {
     }
 });
 
-document.getElementById("loginForm").addEventListener("submit", (event) => {
-    event.preventDefault();
-    const page = window.location.pathname.split("/").pop();
-console.log(window.location.pathname);
-console.log(page);
-    let redirectPath;
-    switch (page) {
-        case "login.html":
-            redirectPath = "../student-side-main/dashboard.php";
-            break;
-        case "admin-login.html":
-            redirectPath = "../admin-side-main/admin-dashboard.html";
-            break;
-        case "officer-login.html":
-            redirectPath = "../org-side-main/officer-dashboard.html";
-            break;
-    }
+// document.getElementById("loginForm").addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const page = window.location.pathname.split("/").pop();
+// console.log(window.location.pathname);
+// console.log(page);
+//     let redirectPath;
+//     switch (page) {
+//         case "login.html":
+//             redirectPath = "../student-side-main/dashboard.php";
+//             break;
+//         case "admin-login.html":
+//             redirectPath = "../admin-side-main/admin-dashboard.html";
+//             break;
+//         case "officer-login.html":
+//             redirectPath = "../org-side-main/officer-dashboard.html";
+//             break;
+//     }
 
-    window.location.href = redirectPath;
-});
+//     window.location.href = redirectPath;
+// });
