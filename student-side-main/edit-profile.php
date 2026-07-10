11995cb3bcc41436774370fcdc38dead9a72f6bb
HiddenGold <104958576+HiddenG0ld@users.noreply.github.com>
2026-07-10 21:28:55 +0800

    Update edit-profile.php

--- student-side-main/edit-profile.php
+++ student-side-main/edit-profile.php
@@ -13,7 +13,7 @@
         <nav class="navbar">
 
             <div class="nav-left"> 
-                <a href="dashboard.php" class="logo-link">
+                <a href="dashboard.php" class="nav-left">
                     <div><img class="logo" src="img/WhatsUpDLSULogo.png" alt="Logo"></div>
                     <span class="logo-text">WhatsUpDLSU</span>
                 </a>
