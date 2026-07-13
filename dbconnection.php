d24f34ad3f9567e8d075da39f9c32a677b2ea987
Te0866 <johann_te@dlsu.edu.ph>
2026-07-13 20:42:20 +0800

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "whatsupdlsu");
+$conn = mysqli_connect("localhost", "root", "iPqfwfLp5FKk", "whatsupdlsu");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
