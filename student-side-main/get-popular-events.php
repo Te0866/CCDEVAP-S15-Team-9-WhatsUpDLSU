d418ce517c1a7357a94c7c0c5dfa92c98055e51b
Te0866 <johann_te@dlsu.edu.ph>
2026-07-14 08:13:59 +0800

    Update get-popular-events.php

--- student-side-main/get-popular-events.php
+++ student-side-main/get-popular-events.php
@@ -6,10 +6,10 @@ header("Content-Type: application/json");
 $sql = "
 SELECT
     e.TITLE,
-    COUNT(i.EVENT_ID) AS interested
+    COUNT(ei.EVENT_ID) AS interested
 FROM event e
-LEFT JOIN interested_events i
-ON e.EVENT_ID = i.EVENT_ID
+LEFT JOIN event_interest ei
+    ON e.EVENT_ID = ei.EVENT_ID
 WHERE e.APPROVAL_STATUS = 'APPROVED'
 GROUP BY e.EVENT_ID
 ORDER BY interested DESC
@@ -20,7 +20,7 @@ $result = mysqli_query($conn, $sql);
 
 $data = [];
 
-while ($row = mysqli_fetch_assoc($result)) {
+while($row = mysqli_fetch_assoc($result)){
     $data[] = $row;
 }
 
