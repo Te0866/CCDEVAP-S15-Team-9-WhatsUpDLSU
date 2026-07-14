1b8af344daa401f10861bf4edfe9f872d27cca23
Te0866 <johann_te@dlsu.edu.ph>
2026-07-14 23:00:44 +0800

    Update events.js

--- student-side-main/js/events.js
+++ student-side-main/js/events.js
@@ -276,7 +276,7 @@ document.getElementById("interestedBtn").addEventListener("click", async () => {
 
     try {
 
-        const response = await fetch("toggle-interest.php", {
+        const response = await fetch("api/add-interest.php", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
