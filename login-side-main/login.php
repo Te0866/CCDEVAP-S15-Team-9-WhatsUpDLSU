58068f138eb3dddcceb2b65325488e6946a9b725
Te0866 <johann_te@dlsu.edu.ph>
2026-07-14 12:32:50 +0800

    Update login.php

--- login-side-main/login.php
+++ login-side-main/login.php
@@ -32,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 header("Location: ../org-side-main/officer-dashboard.php");
             }
             else {
-                header("Location: ../student-side-main/index.php");
+                header("Location: ../student-side-main/dashboard.php");
             }
 
             exit();
