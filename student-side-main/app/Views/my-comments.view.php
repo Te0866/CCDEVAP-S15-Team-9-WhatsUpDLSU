3503d61c6a07f70e95f7f4bd65aad8b58fd92c69
Te0866 <johann_te@dlsu.edu.ph>
2026-08-05 15:02:23 +0800

    Update my-comments.view.php

--- student-side-main/app/Views/my-comments.view.php
+++ student-side-main/app/Views/my-comments.view.php
@@ -6,6 +6,7 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>My Comments - WhatsUpDLSU</title>
 <link rel="stylesheet" href="../assets/styles/student/darkmode.css">
 <link rel="stylesheet" href="../assets/styles/student/my-comments.css">
